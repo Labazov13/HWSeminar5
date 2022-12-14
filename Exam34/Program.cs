7aa922007bef6c08b3e8473a45aa9629f1bbcf72
Nikolay <labazopelastra@gmail.com>
2022-12-14 20:11:19 +0700

    Add new file Exam34

---
+++ Exam34/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] GetAndPrintArray()
+{
+    System.Console.Write("Введите количество элементова массива: ");
+    int ArraySize = int.Parse(Console.ReadLine()!);
+    int[] array = new int[ArraySize];
+    for (int i = 0; i < ArraySize; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        System.Console.Write(" " + array[i] + " ");
+    }
+    return array;
+}
+
+void SearchEven(int[] array)
+{   int count=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;   
+        }
+    }
+    System.Console.WriteLine("Четных чисел в данном массиве: "+count);
+}
+
+int[] array = GetAndPrintArray();
+System.Console.WriteLine();
+SearchEven(array);
